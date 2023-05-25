7f20105ec5a60ae847a82f7d72bc4a05b428e63b
Filippobuda <filippo.buda@mail.polimi.it>
2023-05-25 12:36:06 +0000

    Pushino

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 #!/bin/bash
->>>>>>> 0b5a9b014369abe1249298d66b863a56aaeaaa96
 
 bin/Debug/Helloworld
-=======
-Helloworld
->>>>>>> 67027ca6e9c34280c1557c975471853e79f291ec
