a5c9e8f21a06364de0cb390a9d0f959df2fe78f6
Filippobuda <filippo.buda@mail.polimi.it>
2023-05-29 21:09:41 +0000

    lol

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-mkdir build && cd build && g++ ../googletest/samples/sample1.cc  ../googletest/samples/sample1_unittest.cc -o output -lpthread -lgtest -lgtest_main
+mkdir build && cd build && g++ ../googletest/src/gtest_main.cc ../googletest/samples/sample1.cc  ../googletest/samples/sample1_unittest.cc -o output -lpthread -lgtest 
