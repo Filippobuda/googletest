f1e9a3da5299f713f6a62a1fb9a4f306731098df
Filippobuda <filippo.buda@mail.polimi.it>
2023-05-31 07:59:09 +0000

    Altra regex

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-mkdir build && cd build && g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/*[1-9]*.cc -o output -lpthread -lgtest 
+mkdir build && cd build && g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/*[1-9][a-z]+.cc -o output -lpthread -lgtest 
