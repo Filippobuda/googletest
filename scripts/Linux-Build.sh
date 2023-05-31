7c2be4488acd4185b7413d55bfe15946b5d80643
Filippobuda <filippo.buda@mail.polimi.it>
2023-05-31 10:55:06 +0000

    Prova con solo sample 5, fino a 4 OK

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -7,16 +7,16 @@ pwd
 cd  src/build
 pwd
 #questi invece sono da dentro googletest/src/build provo a farli separati
-g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample1.cc ../../googletest/samples/sample1_unittest.cc -o output1 -lpthread -lgtest
-echo "Sample 1 built"
-g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample2*.cc -o output2 -lpthread -lgtest
-echo "Sample 2 built"
-g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample3_unittest.cc -o output3 -lpthread -lgtest
-echo "Sample 3 built"
-g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample4*.cc -o output4 -lpthread -lgtest
-echo "Sample 4 built"
-# g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample1.cc ../../googletest/samples/sample5_unittest.cc -o output5 -lpthread -lgtest
-# echo "Sample 5 built"
+# g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample1.cc ../../googletest/samples/sample1_unittest.cc -o output1 -lpthread -lgtest
+# echo "Sample 1 built"
+# g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample2*.cc -o output2 -lpthread -lgtest
+# echo "Sample 2 built"
+# g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample3_unittest.cc -o output3 -lpthread -lgtest
+# echo "Sample 3 built"
+# g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample4*.cc -o output4 -lpthread -lgtest
+# echo "Sample 4 built"
+g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample1.cc ../../googletest/samples/sample5_unittest.cc -o output5 -lpthread -lgtest
+echo "Sample 5 built"
 # g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample6*.cc -o output6 -lpthread -lgtest
 # echo "Sample 6 built"
 # g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample7*.cc -o output7 -lpthread -lgtest
