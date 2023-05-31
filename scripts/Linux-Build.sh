1a20d00199525532efa0a39409716458b2c8f55d
Filippobuda <filippo.buda@mail.polimi.it>
2023-05-31 08:01:45 +0000

    Ho escluso il test 9 e 10

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-mkdir build && cd build && g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/*[1-9][a-z]+.cc -o output -lpthread -lgtest 
+mkdir build && cd build && g++ ../../googletest/src/gtest_main.cc ../../googletest/samples/sample1*.cc ../../googletest/samples/sample2*.cc ../../googletest/samples/sample3*.cc ../../googletest/samples/sample4*.cc ../../googletest/samples/sample5*.cc ../../googletest/samples/sample6*.cc ../../googletest/samples/sample7*.cc ../../googletest/samples/sample8*.cc -o output -lpthread -lgtest 
