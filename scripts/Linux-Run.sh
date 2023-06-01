071bfbbc5f758d6a7297b7402322e3e14ec1070d
Filippobuda <filippo.buda@mail.polimi.it>
2023-06-01 08:39:27 +0000

    Prova con cmake

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 cd src/build
-./output1
-./output2
-./output3
-./output4
-./output5
-./output6
-./output7
-./output8
-./output9
-./output10
+./exec
+./exec2
+./exec3
+# cd src/build
+# ./output1
+# ./output2
+# ./output3
+# ./output4
+# ./output5
+# ./output6
+# ./output7
+# ./output8
+# ./output9
+# ./output10
