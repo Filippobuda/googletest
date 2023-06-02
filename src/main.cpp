9853723ba837a8ffe955ac083153fe7937c0596f
gen7 <giuseppe2.gentile@mail.polimi.it>
2023-06-02 21:30:41 +0200

    Usa file in input da riga di comando (./exec ../matrix.txt)
    controlla se ogni numero della matrice è primo

--- src/main.cpp
+++ src/main.cpp
@@ -3,118 +3,70 @@
 #include <vector>
 #include <sstream>
 #include <numeric>
-
 #include <gtest/gtest.h>
+#include "../googletest/samples/sample1.h"
+
+std::string matrixFileName;
 
 
-// Function to read matrix from file
-std::vector<std::vector<float>> readMatrixFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    std::vector<std::vector<float>> matrix;
-    std::string line;
+class Matrix {
+    private:
+        int rows;
+        int columns;
+        std::vector<std::vector<int>> data;
 
-    while (std::getline(file, line)) {
-        std::vector<float> row;
-        float value;
-        std::istringstream iss(line);
+    public:
+        Matrix(const std::string& filename) {
+            std::string line;
+            std::ifstream file(filename);
+            
+            while (std::getline(file, line)) {
+                std::vector<int> row;
+                int value;
+                std::istringstream iss(line);
 
-        while (iss >> value) {
-            row.push_back(value);
+                while (iss >> value) {
+                    row.push_back(value);
+                }
+
+                data.push_back(row);
+            }
         }
 
-        matrix.push_back(row);
-    }
+        void print() const {
+            for (const auto& row : data) {
+                for (int value : row) {
+                    std::cout << value << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
 
-    return matrix;
-}
-
-// Function to read vector from file
-std::vector<float> readVectorFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    std::vector<float> vector;
-    std::string line;
-
-    while (std::getline(file, line)) {
-        float value = std::stof(line);
-        vector.push_back(value);
-    }
-
-    return vector;
-}
-
-// Function to perform matrix-vector multiplication
-std::vector<float> matrixVectorMultiplication(const std::vector<std::vector<float>>& matrix, const std::vector<float>& vector) {
-    std::vector<float> result(matrix.size(), 0.0);
-
-    for (size_t i = 0; i < matrix.size(); i++) {
-        result[i] = std::inner_product(matrix[i].begin(), matrix[i].end(), vector.begin(), 0.0);
-        // for (size_t j = 0; j < matrix[i].size(); j++) {
-        //     result[i] += matrix[i][j] * vector[j];
-        // }
-    }
-
-    return result;
-}
-
-
-// Test fixture for matrix-vector multiplication
-class MatrixVectorMultiplicationTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        // Set up test data
-        matrix = { { 1.0, 2.0, 3.0 },
-                   { 4.0, 5.0, 6.0 },
-                   { 7.0, 8.0, 9.0 } };
-        vector = { 1.0, 2.0, 3.0 };
-        expected_result = { 14.0, 32.0, 50.0 };
-    }
-
-    // Declare variables for test data and expected result
-    std::vector<std::vector<float>> matrix;
-    std::vector<float> vector;
-    std::vector<float> expected_result;
+        int getRows(){
+            return rows;
+        }
+        int getCols(){
+            return columns;
+        }
+        int get(int i, int j) { return data.at(i).at(j);}
 };
 
-// Test case to check matrix-vector multiplication
-TEST_F(MatrixVectorMultiplicationTest, MultiplicationTest) {
-    // Call the matrix-vector multiplication function
-    std::vector<float> result = matrixVectorMultiplication(matrix, vector);
-
-    // Check if the result matches the expected result
-    ASSERT_EQ(result, expected_result);
-}
-
-// Entry point for running the tests
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
-
-/*
-int main(int argc, char *argv[]) {
-    std::string matrixFile = "matrix.txt";    // Update with your matrix file path
-    std::string vectorFile = "vector.txt";    // Update with your vector file path
-    std::string outputFile = "result.txt";    // Update with the desired output file path
-
-    std::vector<std::vector<float>> matrix = readMatrixFromFile(matrixFile);
-    std::vector<float> vector = readVectorFromFile(vectorFile);
-
-    std::vector<float> result = matrixVectorMultiplication(matrix, vector);
-
-    // Write the result to the output file
-    std::ofstream outFile(outputFile);
-    if (outFile.is_open()) {
-        for (const auto& value : result) {
-            outFile << value << "\n";
+TEST(MatrixTest, IsMatrixPrime) {
+    Matrix matrix(matrixFileName);
+    for (int i = 0; i < matrix.getRows(); i++) {
+        for (int j = 0; j < matrix.getCols(); j++) {
+            ASSERT_TRUE(IsPrime(matrix.get(i, j)));
         }
-        outFile.close();
-        std::cout << "Result written to " << outputFile << std::endl;
     }
-    else {
-        std::cerr << "Unable to open output file!" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        matrixFileName = argv[1];
+        testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    } else {
+        std::cerr << "No matrix file provided!" << std::endl;
         return 1;
     }
-
-    return 0;
-}*/
+}
